fb49f19ddf19e30f4b8f269c554d2b3df5fc08ce
britonathally903-cell <britonathally903@gmail.com>
2026-05-30 11:45:39 -0300

    Add files via upload
    loop-while

---
+++ 06_loop-while/atividade01_loop_whide.py
@@ -0,0 +1,10 @@
+#autor Náthally brito
+#projeto:Loop while
+numero = int(input('digite a tabuada que deseja?'))
+i = 1 
+
+while i <= 10:
+    print(f"valor de i antes da interação")
+    print(f"{numero} x {i} = {numero * i}")
+    i = i + 1 
+    print(f"valor do i depois da intereção {1}")
